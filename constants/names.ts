402359c52f5f2f2eb6b19bc400f445a64baa017a
snehilshah <snehilshah27@gmail.com>
2025-06-01 00:11:05 +0530

    :exclamation: backup for arch installation btw

---
+++ constants/names.ts
@@ -0,0 +1,27 @@
+type Client = {
+  name: string;
+  mobile: string;
+};
+
+export const clients: Client[] = [
+  {
+    name: "Snehil Shah",
+    mobile: "8169558980",
+  },
+  {
+    name: "Ronak Shah",
+    mobile: "9930649797",
+  },
+  {
+    name: "Harshal Shah",
+    mobile: "9321878531",
+  },
+  {
+    name: "Sulekha Shah",
+    mobile: "9930649798",
+  },
+  {
+    name: "MPIT",
+    mobile: "8425846202",
+  },
+];
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 import puppeteer from "puppeteer";
 import { userInput } from "./helpers";
-
-const client = "9930649798";
+import { clients } from "../constants/names";
 
 async function main() {
   const browser = await puppeteer.launch({
@@ -14,28 +13,43 @@ async function main() {
   await page.goto("https://web.whatsapp.com/");
 
   await userInput();
+  let counter = 0;
+  for (const client of clients) {
+    counter++;
+    if (counter % 15 === 0) {
+      await new Promise(resolve =>
+        setTimeout(resolve, Math.random() * 10000 + 10000)
+      );
+    }
+    console.log(`Sending message to ${client.name}...`);
+    try {
+      await new Promise(resolve =>
+        setTimeout(resolve, Math.random() * 2000 + 1000)
+      );
+      await page.click(".lexical-rich-text-input", {
+        delay: Math.random() * 100 + 1000,
+      });
+      await new Promise(resolve =>
+        setTimeout(resolve, Math.random() * 2000 + 1000)
+      );
+      await page.keyboard.type(client.mobile);
+      await page.keyboard.press("Enter");
+      await new Promise(resolve =>
+        setTimeout(resolve, Math.random() * 2000 + 1000)
+      );
+      const textToSend = `Hello ${client.name}, I am automatically sending this text!`;
+      await page.keyboard.type(textToSend);
+      await page.keyboard.press("Enter");
+    } catch (error) {
+      console.error(`Error sending message to ${client.name}:`, error);
+      break;
+    }
+    await new Promise(resolve => setTimeout(resolve, 2000));
+  }
+  console.log("All messages sent successfully!");
+  console.log("closing browsr...");
 
-  await page.waitForSelector(".lexical-rich-text-input", { timeout: 60000 });
-
-  await page.click(".lexical-rich-text-input");
-  await page.keyboard.type(client);
-  await page.keyboard.press("Enter");
-
-  //   const inputElements = await page.$$(".lexical-rich-text-input");
-
-  //   if (inputElements.length >= 2) {
-  //     await inputElements[1].click();
-  //     console.log("Clicked on the second input element");
-  //   } else {
-  //     console.log(`Found only ${inputElements.length} input element(s)`);
-  //   }
-  
-  
-
-  const textToSend = "Hello, I am automatically sending this text!";
-
-  await page.keyboard.type(textToSend);
-  await page.keyboard.press("Enter");
+  await browser.close();
 }
 
 main();
